c6686e12ba9beddcec6f7beb94b44f620b42f335
Abramsikn <Abramsikn@gmail.com>
2018-11-09 10:44:21 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '**', component: AdminDashboardComponent },
+      { path: 'dashboard', component: AdminDashboardComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
